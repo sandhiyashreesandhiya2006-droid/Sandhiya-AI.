5664d63f7c6daedd5e653aa012b295b12494d8dc
sandhiyashreesandhiya2006-droid <sandhiyashreesandhiya2006@gmail.com>
2026-06-13 13:06:04 +0530

    Update ai_chatbot.py

--- ai_chatbot.py
+++ ai_chatbot.py
@@ -24,7 +24,7 @@ API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 # =========================
 # TITLE
