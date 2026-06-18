0a4615b9b6746c48d54537aa85b5d0bba607a94e
sandhiyashreesandhiya2006-droid <sandhiyashreesandhiya2006@gmail.com>
2026-06-18 14:46:42 +0530

    Update ai_chatbot.py

--- ai_chatbot.py
+++ ai_chatbot.py
@@ -120,8 +120,8 @@ with st.sidebar:
   
     if st.button("Generate Interview Questions"):
     
-    prompt = f"""
-    Generate 10 interview questions for {job_role}.
+        prompt = f"""
+        Generate 10 interview questions for {job_role}.
     Include:
     1. Technical Questions
     2. Scenario Based Questions
