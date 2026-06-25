583792e5903aebb756ed06b0138572067f91957a
sandhiyashreesandhiya2006-droid <sandhiyashreesandhiya2006@gmail.com>
2026-06-25 15:38:22 +0530

    Update ai_chatbot.py

--- ai_chatbot.py
+++ ai_chatbot.py
@@ -15,6 +15,10 @@ if "attempts" not in st.session_state:
 
 def login():
     st.title("🔐 Login")
+    
+    if st.session_state.attempts >= 3:
+    st.error("🚫 Too many failed attempts. Refresh the app to try again.")
+    st.stop()
 
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
